496948675c6dcea5e78ab2aeb9d0d94081e3a2cb
Vladyslav Fedorov <gkorchun@gmail.com>
2023-06-29 16:06:46 +0300

    Operator precendence and math functions

---
+++ 8_operator_precedence_and_math_functions.py
@@ -0,0 +1,27 @@
+import math
+# https://docs.python.org/3/library/math.html
+
+x = 10 + 3 * 2 ** 2
+print(x)  # 22
+
+# operations orders:
+# parenthesis
+# exponentiation
+# multiplication or division
+# addition or subtraction
+
+# round()
+x = 2.9
+print(round(x))  # 3
+
+# abs()
+x = -2.9
+print(abs(x))  # 2.9
+
+# math.ceil()
+x = 2.9
+print(math.ceil(x))  # 3
+
+# math.floor()
+x = 2.9
+print(math.floor(x))  # 2
